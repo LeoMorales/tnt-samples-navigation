ed38281550dd0dc8af8bd3872fcf42fd58ed98d1
Leonardo Morales <leo.moralesr23@gmail.com>
2021-05-04 12:38:13 -0300

    project load

---
+++ README.md
@@ -0,0 +1,5 @@
+# tnt-samples-navigation
+
+Proyecto base para implementar la funcionalidad de navegación
+
+![imagen](https://user-images.githubusercontent.com/2643388/116957692-e0090680-ac6e-11eb-94c9-9b6f2a937b90.png)
---
+++ app/src/main/java/unpsjb/ing/tnt/anotadortruconav/InicioFragment.kt
@@ -0,0 +1,34 @@
+package unpsjb.ing.tnt.anotadortruconav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import unpsjb.ing.tnt.anotadortruconav.databinding.FragmentInicioBinding
+
+class InicioFragment : Fragment() {
+
+    private var _binding: FragmentInicioBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInicioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
