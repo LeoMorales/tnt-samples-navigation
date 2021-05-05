a883ad48babb5cc30d0fd4154fabbdc4784342c5
Leonardo Morales <leo.moralesr23@gmail.com>
2021-05-05 14:04:15 -0300

    adds navigation drawer

--- app/src/main/java/unpsjb/ing/tnt/anotadortruconav/InicioFragment.kt
+++ app/src/main/java/unpsjb/ing/tnt/anotadortruconav/InicioFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import unpsjb.ing.tnt.anotadortruconav.databinding.FragmentInicioBinding
 
@@ -28,7 +29,7 @@ class InicioFragment : Fragment() {
 
         binding.button.setOnClickListener {
             val action = InicioFragmentDirections.playAction("AAA", "BBB")
-            findNavController().navigate(action)
+            it.findNavController().navigate(action)
         }
         return view
     }
--- app/src/main/java/unpsjb/ing/tnt/anotadortruconav/MainActivity.kt
+++ app/src/main/java/unpsjb/ing/tnt/anotadortruconav/MainActivity.kt
@@ -2,10 +2,42 @@ package unpsjb.ing.tnt.anotadortruconav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import unpsjb.ing.tnt.anotadortruconav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        drawerLayout = binding.drawerLayout
+        // val navController = this.findNavController(R.id.theNavHostFragment)  // <-- no funciona acá como en onSupportNavigateUp
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.theNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        setContentView(view)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.theNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+        //return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
     }
 }
